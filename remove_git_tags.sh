7b5c79b0cd10d5b3eedcf26d93286156c8a4b443
Gansop, Armel <armel.gansop@kronos.com>
2020-05-19 15:49:13 -0400

    updated

---
+++ remove_git_tags.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script removes unwanted tags
+
+for i in $(git tag -l)
+do
+	if [ $i != "1.1.0" ]
+	then
+		if [ $i != "1.1.1" ]
+		then
+			if [ $i != "1.2.0" ]
+			then
+				if [ $i != "sprint4-demo" ]
+				then
+					git push --delete origin $i
+				fi
+			fi
+
+		fi
+
+	fi
+
+done
