0ed18fdbc5b6d8e946eefc3043ed74f2f0f072f4
Gansop, Armel <armel.gansop@kronos.com>
2020-09-10 17:39:35 -0400

    sample

--- my_scripts/checking_git_repo.sh
+++ my_scripts/checking_git_repo.sh
@@ -4,13 +4,17 @@
 #checking if the repo was updated or not
 
 
-git status
+git status -uno 
 
 if [[ $? == 0 ]]
 then
+        git add .
+        git commit -m "sample"
+        git push
         echo "Nothing to do!!!"
+fi
+
 #elif [[ $(git rev-parse --abbrev-ref HEAD) ]]
 #echo "You are in the $(git rev-parse --abbrev-ref HEAD) branch"
-fi
 
 
