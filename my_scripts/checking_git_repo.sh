9a985f5ecd38efd1409f21611d58a1b1441bd73a
Gansop, Armel <armel.gansop@kronos.com>
2020-09-11 15:07:34 -0400

    added

--- my_scripts/checking_git_repo.sh
+++ my_scripts/checking_git_repo.sh
@@ -8,6 +8,7 @@ script=`git status -uno | grep -i "nothing to commit" | awk '{print $1 " "  $2 "
 if [[ $script == "nothing to commit" ]]
 then
     echo "There is nothing to do"
-    echo "Bye Bye!!!!"
     exit 1
 fi
+
+echo "Hello"
